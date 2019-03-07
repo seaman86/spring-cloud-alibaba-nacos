1edebf610366fe8a59cca0184232d35db96a908b
wangqiuhua <wangqihua@xforceplus.com>
2019-03-07 15:25:15 +0800

    add code

---
+++ alibaba-nacos/alibaba-nacos-discovery-server/src/main/java/com/seaman/nacos/TestApplication.java
@@ -0,0 +1,36 @@
+package com.seaman.nacos;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 版权：    上海云砺信息科技有限公司
+ * 创建者:   wangqiuhua
+ * 创建时间:  2019-03-07 15:01
+ * 功能描述:
+ * 修改历史:
+ */
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+
+    @Slf4j
+    @RestController
+    static class TestController {
+        @GetMapping("/hello")
+        public String hello(@RequestParam String name) {
+            log.info("invoked name = " + name);
+            return "hello " + name;
+        }
+    }
+}
